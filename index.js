96faf85467726be85993dda6254a3f9492b4b90e
Alexis S Liu <syl1350@gmail.com>
2021-07-19 23:39:46 -0400

    commit tree 618a254c3687adf6ae7ee73acf5cfe33156d1d98

---
+++ index.js
@@ -0,0 +1,15 @@
+//a function expression called divide
+const divide = function(){ 
+  return 2000 / 100 
+}
+
+//arrow function called square
+const square = num => num * num
+  //takes one parameter and multiples it by itself
+  //since only one parameter, no need for parentheses (num) or {}
+  //does not need return
+
+  //arrow function called add
+  const add = (num1, num2) => num1 + num2;
+     //doesnt need return because it's only one expession
+
